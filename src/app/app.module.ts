c829f964ca781f340ec0582233d8aaaaa235ad7e
Amit Rana <Amirana@14325@gmail.com>
2023-02-05 15:39:31 +0530

    Added by AMIT RANA : added Footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { SliderComponent } from './slider/slider.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SliderComponent
+    SliderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
